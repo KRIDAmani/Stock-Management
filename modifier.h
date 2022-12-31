130994cf3e5f2dc98714d91e18372697a4a16563
KRIDAmani <113017005+KRIDAmani@users.noreply.github.com>
2022-12-31 01:11:47 +0100

    Update modifier.h

--- modifier.h
+++ modifier.h
@@ -27,8 +27,8 @@ void modif ()
         if (rep[0]=='o')
             {
                 FILE* Fich, *F;
-                F=fopen("C:\\Users\\amani\\OneDrive\\Bureau\\mini projet\\main1.c\\stock.txt","r");
-                Fich=fopen("C:\\Users\\amani\\OneDrive\\Bureau\\mini projet\\main1.c\\stock1.txt","w");
+                F=fopen("stock.txt","r");
+                Fich=fopen("stock1.txt","w");
                 do
                 {
                     produit produit;
@@ -53,8 +53,8 @@ void modif ()
                 }while(!feof(F));
                 fclose(F);
                 fclose(Fich);
-                F=fopen("C:\\Users\\amani\\OneDrive\\Bureau\\mini projet\\main1.c\\stock.txt","w");
-                Fich=fopen("C:\\Users\\amani\\OneDrive\\Bureau\\mini projet\\main1.c\\stock1.txt","r");
+                F=fopen("stock.txt","w");
+                Fich=fopen("stock1.txt","r");
                 do
                 {
                     produit prod;
